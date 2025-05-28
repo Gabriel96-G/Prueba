938126ddb4577360d822b56f626c36ace03c0ac9
Gabriel Medrano <amedrano9624@gmail.com>
2025-05-28 02:26:27 +0000

    S02

--- Ejemplo08.java
+++ Ejemplo08.java
@@ -1,1 +1,21 @@
+package semana02;
 
+import utilitarios.Utils;
+
+public class Ejercicio08 {
+
+    public static final double PORCENTAJE_IMPUESTO = 0.30;
+    public static final double GANANCIA_VENDEDOR = 0.10;
+
+    public static void main(String[] args) {
+
+        double costoUnitario = Utils.leerDoble("Ingresar precio del vehículo");
+
+        double precioFinalImpuesto = costoUnitario * PORCENTAJE_IMPUESTO;
+        double precioFinalVendedor = costoUnitario * GANANCIA_VENDEDOR;
+        double precioFinal = costoUnitario + precioFinalImpuesto + precioFinalVendedor;
+
+        // Salida
+        System.out.println("Precio final del vehículo: " + precioFinal);
+    }
+}
